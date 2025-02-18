edaffa00d9d0be4e6c4d4e8469647f78773ef269
Aybar Özcan <ybrozcan@gmail.com>
2025-02-19 00:50:55 +0300

    Add project files.

---
+++ HelpDesk.Core/Entities/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace HelpDesk.Core.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ HelpDesk.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpDesk.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
