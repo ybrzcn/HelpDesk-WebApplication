6dbec05764226eec70416fd299a119224214259b
Aybar Özcan <ybrozcan@gmail.com>
2025-03-11 00:24:06 +0300

    Ticket Attachments API completed

---
+++ HelpDesk.API/Controllers/TicketAttachmentController.cs
@@ -0,0 +1,84 @@
+﻿using HelpDesk.Core.Entities;
+using HelpDesk.Infrastructure.Dtos;
+using HelpDesk.Infrastructure.Interfaces;
+using HelpDesk.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketAttachmentController : ControllerBase
+    {
+        private readonly ITicketAttachmentRepository _ticketAttachmentRepository;
+
+        public TicketAttachmentController(ITicketAttachmentRepository ticketAttachmentRepository)
+        {
+            _ticketAttachmentRepository = ticketAttachmentRepository;
+        }
+
+        [HttpGet("GetTicketAttachments")]
+        [ProducesResponseType(200, Type = typeof(List<TicketAttachment>))]
+        public async Task<IActionResult> GetTicketAttachments()
+        {
+            var value = await _ticketAttachmentRepository.GetAllAsync();
+            return Ok(value);
+        }
+
+        [HttpGet("GetTicketAttachmentById/{id}")]
+        [ProducesResponseType(200, Type = typeof(TicketAttachment))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetTicketAttachmentById(Guid id)
+        {
+            var value = await _ticketAttachmentRepository.GetByIdAsync(id);
+            if (value == null) return NotFound("Ticket Attachment not found.");
+            return Ok(value);
+        }
+
+        [HttpGet("GetTicketAttachmentByTicketId/{ticketId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<TicketAttachmentDto>))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetCommentsByRecipeId(Guid ticketId)
+        {
+            var values = await _ticketAttachmentRepository.GetTicketAttachmentByTicketAsync(ticketId);
+            if (values == null || !values.Any())
+                return NotFound("Ticket Attachment not found.");
+
+            return Ok(values);
+        }
+
+        [HttpPost("CreateTicketAttachment")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateTicketAttachment(TicketAttachmentDto dto)
+        {
+            await _ticketAttachmentRepository.CreateAsync(dto);
+            return Ok("Ticket Attachment created successfully.");
+        }
+
+        [HttpDelete("DeleteTicketAttachment/{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DeleteTicketAttachment(Guid id)
+        {
+            await _ticketAttachmentRepository.DeleteAsync(id);
+            return Ok("Ticket Attachment deleted successfully.");
+        }
+
+        [HttpPut("UpdateTicketAttachment/{id}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> UpdateTicketAttachment(Guid id, TicketAttachmentDto dto)
+        {
+            if (id != dto.Id)
+            {
+                return BadRequest("ID mismatch.");
+            }
+
+            await _ticketAttachmentRepository.UpdateAsync(dto);
+            return Ok("Ticket Attachment updated successfully.");
+        }
+    }
+}
---
+++ HelpDesk.Infrastructure/Repositories/TicketAttachmentRepository.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using HelpDesk.Core.Entities;
+using HelpDesk.Infrastructure.Contexts;
+using HelpDesk.Infrastructure.Dtos;
+using HelpDesk.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk.Infrastructure.Repositories
+{
+    public class TicketAttachmentRepository : ITicketAttachmentRepository
+    {
+        private readonly StoreContext _storeContext;
+        private readonly IMapper _mapper;
+
+        public TicketAttachmentRepository(StoreContext storeContext, IMapper mapper)
+        {
+            _storeContext = storeContext;
+            _mapper = mapper;
+        }
+
+        public async Task CreateAsync(TicketAttachmentDto dto)
+        {
+            var value = _mapper.Map<TicketAttachment>(dto);
+            await _storeContext.AddAsync(value);
+            await SaveAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var value = await GetByIdAsync(id);
+            if (value != null)
+            {
+                var ticketAttachment = await _storeContext.TicketAttachments.FindAsync(id);
+                if (ticketAttachment != null)
+                {
+                    _storeContext.Remove(ticketAttachment);
+                    await SaveAsync();
+                }
+            }
+        }
+
+        public async Task<List<TicketAttachmentDto>> GetAllAsync()
+        {
+            return await _storeContext.TicketAttachments
+                .OrderBy(x => x.CreatedDate)
+                .Select(x => _mapper.Map<TicketAttachmentDto>(x))
+                .ToListAsync();
+        }
+
+        public async Task<TicketAttachmentDto?> GetByIdAsync(Guid id)
+        {
+            var value = await _storeContext.TicketAttachments.FirstOrDefaultAsync(x => x.Id == id);
+            return _mapper.Map<TicketAttachmentDto>(value);
+        }
+
+        public async Task<IEnumerable<TicketAttachmentDto>> GetTicketAttachmentByTicketAsync(Guid id)
+        {
+            return await _storeContext.TicketAttachments
+                .Where(x => x.TicketId == id)
+                .Select(x => _mapper.Map<TicketAttachmentDto>(x))
+                .ToListAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _storeContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TicketAttachmentDto dto)
+        {
+            var value = _mapper.Map<TicketAttachment>(dto);
+            _storeContext.Update(value);
+            await SaveAsync();
+        }
+    }
+}
