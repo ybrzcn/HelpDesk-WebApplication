d33ace7fab89095723d4e327747e25d9f0fb444d
Aybar Özcan <ybrozcan@gmail.com>
2025-03-26 00:42:50 +0300

    Ticket Comment API

---
+++ HelpDesk.API/Controllers/TicketCommentController.cs
@@ -0,0 +1,94 @@
+﻿using HelpDesk.Core.Entities;
+using HelpDesk.Infrastructure.Dtos;
+using HelpDesk.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketCommentController : ControllerBase
+    {
+        private readonly ITicketCommentRepository _ticketCommentRepository;
+
+        public TicketCommentController(ITicketCommentRepository ticketCommentRepository)
+        {
+            _ticketCommentRepository = ticketCommentRepository;
+        }
+
+        [HttpGet("GetTicketComments")]
+        [ProducesResponseType(200, Type = typeof(List<TicketComment>))]
+        public async Task<IActionResult> GetTicketComments()
+        {
+            var value = await _ticketCommentRepository.GetAllAsync();
+            return Ok(value);
+        }
+
+        [HttpGet("GetTicketCommentById/{id}")]
+        [ProducesResponseType(200, Type = typeof(TicketComment))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetTicketCommentById(Guid id)
+        {
+            var value = await _ticketCommentRepository.GetByIdAsync(id);
+            if (value == null) return NotFound("Ticket Comment not found.");
+            return Ok(value);
+        }
+
+        [HttpGet("GetTicketCommentByTicketId/{ticketId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<TicketCommentDto>))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetCommentsByTicketId(Guid ticketId)
+        {
+            var values = await _ticketCommentRepository.GetTicketCommentByTicketAsync(ticketId);
+            if (values == null || !values.Any())
+                return NotFound("Ticket Comment not found.");
+
+            return Ok(values);
+        }
+
+        [HttpGet("GetTicketCommentByAuthorId/{authorId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<TicketCommentDto>))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetCommentByAuthorId(Guid authorId)
+        {
+            var values = await _ticketCommentRepository.GetTicketCommentByAuthorAsync(authorId);
+            if (values == null || !values.Any())
+                return NotFound("Ticket Comment not found.");
+
+            return Ok(values);
+        }
+
+        [HttpPost("CreateTicketComment")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateTicketComment(TicketCommentDto dto)
+        {
+            await _ticketCommentRepository.CreateAsync(dto);
+            return Ok("Ticket Comment created successfully.");
+        }
+
+        [HttpDelete("DeleteTicketComment/{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DeleteTicketComment(Guid id)
+        {
+            await _ticketCommentRepository.DeleteAsync(id);
+            return Ok("Ticket Comment deleted successfully.");
+        }
+
+        [HttpPut("UpdateTicketComment/{id}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> UpdateTicketComment(Guid id, TicketCommentDto dto)
+        {
+            if (id != dto.Id)
+            {
+                return BadRequest("ID mismatch.");
+            }
+
+            await _ticketCommentRepository.UpdateAsync(dto);
+            return Ok("Ticket Comment updated successfully.");
+        }
+    }
+}
---
+++ HelpDesk.Infrastructure/Repositories/TicketCommentRepository.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using HelpDesk.Core.Entities;
+using HelpDesk.Infrastructure.Contexts;
+using HelpDesk.Infrastructure.Dtos;
+using HelpDesk.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk.Infrastructure.Repositories
+{
+    public class TicketCommentRepository : ITicketCommentRepository
+    {
+        private readonly StoreContext _storeContext;
+        private readonly IMapper _mapper;
+
+        public TicketCommentRepository(StoreContext storeContext, IMapper mapper)
+        {
+            _storeContext = storeContext;
+            _mapper = mapper;
+        }
+
+        public async Task CreateAsync(TicketCommentDto dto)
+        {
+            var value = _mapper.Map<TicketComment>(dto);
+            await _storeContext.AddAsync(value);
+            await SaveAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var value = await GetByIdAsync(id);
+            if (value != null)
+            {
+                var ticketComments = await _storeContext.TicketComments.FindAsync(id);
+                if (ticketComments != null)
+                {
+                    _storeContext.Remove(ticketComments);
+                    await SaveAsync();
+                }
+            }
+        }
+
+        public async Task<List<TicketCommentDto>> GetAllAsync()
+        {
+            return await _storeContext.TicketComments
+                .OrderBy(x => x.CreatedDate)
+                .Select(x => _mapper.Map<TicketCommentDto>(x))
+                .ToListAsync();
+        }
+
+        public async Task<TicketCommentDto?> GetByIdAsync(Guid id)
+        {
+            var value = await _storeContext.TicketComments.FirstOrDefaultAsync(x => x.Id == id);
+            return _mapper.Map<TicketCommentDto>(value);
+        }
+
+        public async Task<IEnumerable<TicketCommentDto>> GetTicketCommentByAuthorAsync(Guid id)
+        {
+            return await _storeContext.TicketComments
+                .Where(x => x.AuthorId == id)
+                .Select(x => _mapper.Map<TicketCommentDto>(x))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TicketCommentDto>> GetTicketCommentByTicketAsync(Guid id)
+        {
+            return await _storeContext.TicketComments
+                .Where(x => x.TicketId == id)
+                .Select(x => _mapper.Map<TicketCommentDto>(x))
+                .ToListAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _storeContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TicketCommentDto dto)
+        {
+            var value = _mapper.Map<TicketAttachment>(dto);
+            _storeContext.Update(value);
+            await SaveAsync();
+        }
+    }
+}
